7537eba421e8cc310b12a0cb574c39a1ef5d079e
Hejtmus <34794791+Hejtmus@users.noreply.github.com>
2018-02-23 21:30:15 +0100

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from colorama import init, Fore, Back, Style
+
+print (Fore.BLUE + "  ____                       _    ____      _       ")
+print (" / ___| _ __ ___   __ _ _ __| |_ / ___|__ _| | ___  ")
+print (" \___ \| '_ ` _ \ / _` | '__| __| |   / _` | |/ __| ")
+print ("  ___) | | | | | | (_| | |  | |_| |__| (_| | | (__  ")
+print (" |____/|_| |_| |_|\__,_|_|   \__|\____\__,_|_|\___| V 0.1 ")
+print (Fore.WHITE + "						    ")
