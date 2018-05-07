97a98eab1bf747ebeef3dfc45754e0eab7e07cce
Hejtmus <34794791+Hejtmus@users.noreply.github.com>
2018-05-07 16:23:37 +0200

    Add files via upload

--- resistor.py
+++ resistor.py
@@ -17,40 +17,38 @@ r1 = None
 r2 = None
 
 if tom == 2:
-while r1 == None or r1 == None:
-    try:
-        r1 = float( input("R1 = "))
-        r2 = float( input("R2 = "))
-        rp = rev_val(r1, r2)
-        rs = (r1 + r2)
-    except ValueError as verr:
-        print(verr)
-        r1 = None
-        r2 = None
-    except  TypeError as terr:
-        print(terr)
-        r1 = None
-        r2 = None
+    while r1 is None or r1 is None:
+        try:
+            r1 = float( input("R1 = "))
+            r2 = float( input("R2 = "))
+            rp = rev_val(r1, r2)
+            rs = (r1 + r2)
+        except ValueError as verr:
+            print(verr)
+            r1 = None
+            r2 = None
+        except  TypeError as terr:
+            print(terr)
+            r1 = None
+            r2 = None
 
-elif tom > 2:			#wip
-
-while x != tom:
-    x += 1
-    try:
-        print('')
-        print(Fore.GREEN+"Enter R", x)
-        r = float(input(Fore.WHITE +"Value:" ))
-        rp += (1 / r)
-        rs += r
-    except ValueError as verr:
-        print(verr)
-        r = None
-        x = None
-    except  TypeError as terr:
-        print(terr)
-        r1 = None
-        r2 = None
-
+elif tom > 2:
+    while x != tom:
+        x += 1
+        try:
+            print('')
+            print(Fore.GREEN+"Enter R", x)
+            r = float(input(Fore.WHITE +"Value:" ))
+            rp += (1 / r)
+            rs += r
+        except ValueError as verr:
+            print(verr)
+            r = None
+            x = None
+        except  TypeError as terr:
+            print(terr)
+            r1 = None
+            r2 = None
 
 print ("	")
 print("R  serial =", rs)
